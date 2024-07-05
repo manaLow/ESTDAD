ff5e7f639fcab3b9c2bfc0cd5c8a8e828b700b7c
Manuely Meireles Lemos <104777338+manaLow@users.noreply.github.com>
2024-07-05 16:31:05 -0300

    dgdfgerg
    dlkmsvlmsvlmksvlmdsk

---
+++ exam.c
@@ -0,0 +1,85 @@
+#include "exam.h"
+
+#include <stdio.h>
+#include <time.h>
+
+typedef struct {
+    int id;
+    int patient_id;
+    int rx_id;
+    struct tm* time;
+}Exam;
+
+Exam* create_exam(int id, int patient_id, int rx_id, struct tm *time)
+{    
+    Exam* new_exam = (Exam*)malloc(sizeof(Exam));
+    if (new_exam == NULL){
+        printf("Erro ao alocar memória para o novo exame.");
+        return NULL;
+    }
+
+    //Inicializar as variáveis do exame
+    new_exam -> id = id;
+    new_exam -> patient_id = patient_id;
+    new_exam -> rx_id = rx_id;
+
+    new_exam -> time = (struct tm*)malloc(sizeof(struct tm));
+    if (new_exam -> time == NULL){
+        printf("Erro ao alocar memória para o tempo do exame.");
+        free(new_exam);
+    return NULL;
+    }
+    *(new_exam -> time) = *time;
+}
+
+void destroy_exam(Exam *exam){
+    if (exam != NULL){
+        
+        if (exam -> time != NULL){
+            free(exam -> time);
+        }
+
+        free(exam);
+    }
+}
+
+int get_exam_id(Exam *exam){
+    if (exam != NULL){
+        return exam -> id;
+    }
+    else{
+        printf("Erro! O exame não existe.\n");
+        return NULL;
+    }
+}
+
+int get_exam_patient_id(Exam *exam){
+    if (exam != NULL){
+        return exam -> patient_id;
+    }
+    else{
+        printf("Erro! O exame não existe.\n");
+        return NULL;
+    }
+}
+
+int get_exam_rx_id(Exam *exam){
+    if (exam != NULL){
+        return exam -> rx_id;
+    }
+    else{
+        printf("Erro! O exame não existe.\n");
+        return NULL;
+    }
+}
+
+struct tm* get_exam_time(Exam *exam){
+    if (exam != NULL){
+        return exam -> time;
+    }
+    else{
+        printf("Erro! O exame não existe.\n");
+        return NULL;
+    }
+
+}
---
+++ patient.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "patient.h"
+
+typedef struct {
+  int id;
+  char* name;
+  struct tm* birthdate;
+} Patient;
+
+
+Patient* create_patient(int id, const char *name, struct tm *birthdate){
+    Patient* new_patient = (Patient*)malloc(sizeof(Patient));
+    if (new_patient == NULL)
+    {
+        printf("Erro ao alocar memória para novo paciente.\n");
+        return NULL;
+    }
+    new_patient -> id = id;
+    new_patient -> name = strdup(name);
+    if (new_patient->name == NULL){
+        printf("Erro ao alocar memória para o nome do paciente.\n");
+        free(new_patient);
+        return NULL;
+    }
+
+    new_patient -> birthdate = (struct tm*)malloc(sizeof(struct tm));
+    if (new_patient->birthdate == NULL){
+        printf("Erro ao alocar memória para a data de nascimento do paciente.\n");
+        free(new_patient->name);
+        free(new_patient);
+        return NULL;
+    }
+
+    *(new_patient->birthdate) = *birthdate;
+
+    return new_patient;
+}
+
+void destroy_patient(Patient* patient){
+    if (patient != NULL){
+
+        if (patient->name != NULL){
+            free(patient -> name);
+        }
+
+        if (patient->birthdate != NULL){
+            free(patient -> birthdate);
+        }
+
+        free(patient);
+    }
+}
+
+int get_patient_id(Patient* patient){
+    if (patient != NULL){
+        return patient->id;
+    }
+    else{
+        printf("Erro! O paciente não existe.\n");
+        return -1;
+    }
+}
+
+const char* get_patient_name(Patient* patient){
+    if (patient != NULL){
+        return patient->name;
+    }
+    else{
+        printf("Erro! O paciente não existe.\n");
+        return NULL;
+    }
+}
+
+
+struct tm* get_patient_birthdate(Patient *patient){
+    if (patient != NULL){
+        return patient->birthdate;
+    }
+    else{
+        printf("Erro! O paciente não existe.\n");
+        return NULL;
+    }
+}
