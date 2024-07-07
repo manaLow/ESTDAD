585cc33dee7a2b4300e21fc5c8b98341f8667e98
Manuely Meireles Lemos <104777338+manaLow@users.noreply.github.com>
2024-07-07 16:20:23 -0300

    Update exam.c

--- src/exam.c
+++ src/exam.c
@@ -1,9 +1,10 @@
+// Includes
 #include "../include/exam.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+// Estrutura de Exam
 struct exam {
   int id;
   int patient_id;
@@ -11,9 +12,12 @@ struct exam {
   struct tm *time;
 } exam;
 
+
+// Criar exame
 Exam *create_exam(int id, int patient_id, int rx_id, struct tm *time) {
-  Exam *new_exam = (Exam *)malloc(sizeof(Exam));
-  if (new_exam == NULL) {
+  Exam *new_exam = (Exam *)malloc(sizeof(Exam)); // Alocação de mamória pra estrutura
+  // Verifica se foi alocado
+  if (new_exam == NULL) { 
     printf("Erro ao alocar memória para o novo exame.");
     return NULL;
   }
@@ -23,7 +27,10 @@ Exam *create_exam(int id, int patient_id, int rx_id, struct tm *time) {
   new_exam->patient_id = patient_id;
   new_exam->rx_id = rx_id;
 
+  // Precisa de alocar memória para o tempo
   new_exam->time = (struct tm *)malloc(sizeof(struct tm));
+
+  // Verifica se alocou corretamente, caso não: memória liberada
   if (new_exam->time == NULL) {
     printf("Erro ao alocar memória para o tempo do exame.");
     free(new_exam);
@@ -33,8 +40,9 @@ Exam *create_exam(int id, int patient_id, int rx_id, struct tm *time) {
 
   return new_exam;
 }
-
+// Função para liberar memória de um exame
 void destroy_exam(Exam *exam) {
+  // Sempre verificando se, por acaso, o parametro passado existe
   if (exam != NULL) {
 
     if (exam->time != NULL) {
@@ -44,7 +52,7 @@ void destroy_exam(Exam *exam) {
     free(exam);
   }
 }
-
+// Função para retornar o id do exam
 int get_exam_id(Exam *exam) {
   if (exam != NULL) {
     return exam->id;
@@ -53,7 +61,7 @@ int get_exam_id(Exam *exam) {
     return -1;
   }
 }
-
+// Função para retornar o id do paciente em relação ao exame
 int get_exam_patient_id(Exam *exam) {
   if (exam != NULL) {
     return exam->patient_id;
@@ -62,7 +70,7 @@ int get_exam_patient_id(Exam *exam) {
     return -1;
   }
 }
-
+// Função para retornar o id da maquina usada no exame
 int get_exam_rx_id(Exam *exam) {
   if (exam != NULL) {
     return exam->rx_id;
@@ -71,7 +79,7 @@ int get_exam_rx_id(Exam *exam) {
     return -1;
   }
 }
-
+// Função para retornar o horário que o exame foi feito
 struct tm *get_exam_time(Exam *exam) {
   if (exam != NULL) {
     return exam->time;
